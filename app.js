7cc39d38b182478036b84f13a5c9b601ac9c05d5
Roni Conrado <r.conrado.m@gmail.com>
2025-03-01 17:11:21 -0300

    Atualizando o código.

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Array para armazenar os nomes.
+let amigos = [];
+
+// Função para adicionar um amigo à lista.
+function adicionarAmigo() {
+    let imputAmigo = document.getElementById("amigo"); // captura o campo de entrada.
+    let nomeAmigo = imputAmigo.value.trim(); // Obtém o valor, remove espaço extra.
+    
+    // Validação: Verifica se o campo não está vazio.
+    if (!nomeAmigo) {
+        alert("Por favor, insira um nome.")
+        return;
+    }
+    
+    // Adicionar o nome ao array de amigos.
+    amigos.push(nomeAmigo);    
+    imputAmigo.value = "";
+    imputAmigo.focus();
+    atualizarLista();
+}
+
+// Função para atualizar a lista exibida na tela.
+
+function atualizarLista() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = ""; // limpa a lista antes de atualizar.
+
+    // Adicionar cada amigo de forma dinâmica.
+
+    amigos.forEach((nome, index) => {
+        let li = document.createElement("li"); // cria um item de de lista.
+        li.textContent = nome; // define o nome como texto do item.
+        listaAmigos.appendChild(li);
+    })
+    
+}
+
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert("Nenhum amigo adicionado");
+        return;
+    }
+    let sorteado = amigos[Math.floor(Math.random())];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `O amigo sorteado foi: ${sorteado}`;
+}
